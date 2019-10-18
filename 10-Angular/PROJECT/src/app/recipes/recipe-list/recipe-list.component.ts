b0eb1d608b82e2b12e572cd0ab2e0beedf11baa8
maloney8 <marcziur8@gmail.com>
2019-10-18 15:18:08 +0200

    services and routing

--- 10-Angular/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 10-Angular/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {Recipe} from '@angular/PROJECT/src/app/recipes/recipe.model';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,13 @@ import {Recipe} from '@angular/PROJECT/src/app/recipes/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://www.mindmegette.hu/images/213/O/paloc_leves.jpg'),
-    new Recipe('Another recipe', 'This is simply a test', 'http://www.mindmegette.hu/images/213/O/paloc_leves.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+
+  }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
-
 }
--- 10-Angular/PROJECT/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 10-Angular/PROJECT/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
-import {Ingredient} from '@angular/cli/PROJECT/src/app/shared/ingredient.model';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,10 +8,10 @@ import {Ingredient} from '@angular/cli/PROJECT/src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<{Ingredient}>();
-  constructor() { }
+  //@ViewChild('nameInput') nameInputRef: ElementRef;
+  //@ViewChild('amountInput') amountInputRef: ElementRef;
+
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -19,7 +20,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
 
 }
